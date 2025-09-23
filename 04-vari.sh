abe9fc3d1b8198df232078d8e51bd6338333fefc
fayum <shaikfayum@gmail.com>
2025-09-23 14:14:54 +0530

    some messages

--- 04-vari.sh
+++ 04-vari.sh
@@ -1,7 +1,7 @@
 person1=$1
 person2=$2
 echo "$person1:hi,$person2 what are doing "
-echo "$person2:hi,person1 i am working in the mlc "
+echo "$person2:hi, i am working in the mlc "
 echo "$person1:where is your work place $person2"
 echo "$person2:my work place is bengulure $person1"
 echo  "$person1:nice to meet u $person2"
