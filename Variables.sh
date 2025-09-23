14c558acd3e298cfcf12e6514cab48cae9e25427
fayum <shaikfayum@gmail.com>
2025-09-23 13:10:56 +0530

    some message

---
+++ Variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+person1=khaja
+person2=raja
+echo "hi what man whats going on $person2"
+echo "ntg man what are doing $person1"
+echo "where ar u working right now $person2"
+echo "i am workin in the google meta $preson1"
+ 
