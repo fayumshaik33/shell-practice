55fa91b2c407cec45becd9ed97ef750ee3fc8be8
fayum <shaikfayum@gmail.com>
2025-09-25 10:20:03 +0530

    some messages

--- old.sh
+++ old.sh
@@ -2,6 +2,6 @@
 
 echo "this is thing i need to tell ::$@"
 echo "this is the script name ::$0"
-echo "who is running this script ::$user"
-echo "the home directory of the user ::$home"
-echo "current working directory ::$ped"
+echo "who is running this script ::$USER"
+echo "the home directory of the user ::$HOME"
+echo "current working directory ::$PWD"
