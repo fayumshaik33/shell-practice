89eee61b300ea899ab7f3d7f7e4457ec6d0fc2c3
fayum <shaikfayum@gmail.com>
2025-09-25 13:46:08 +0530

    session

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -2,7 +2,7 @@
 number=$1
 if [ $number -lt 9 ]; then
     echo "the number is less than 9"
-ifel [ $number -eq 9 ]; then
+elif [ $number -eq 9 ]; then
     echo "the number is equal to 9"
 else
     echo "the number is bigger to 9"
