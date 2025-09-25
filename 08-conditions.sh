f3410bc65ad820126f9fc2175138f614f3509acd
fayum <shaikfayum@gmail.com>
2025-09-25 15:55:19 +0530

    session

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -1,8 +1,13 @@
-#!/bin/bash
-echo "place enter the number"
-read number
-if [ $(($number % 2)) ]; then 
-    echo "given number $number is even"
-else
-    echo "given number $number is odd"
+#! bin/bash
+idnumber=$(id -u)
+if [ $idnumber -ne 0]; then 
+    echo "error :: place run this script with root"
 fi
+  dnf install mysql -y
+
+if [ $? -ne 0 ]; then 
+    echo "error ::installation failed"
+else 
+    echo "intallation success"
+fi
+  
