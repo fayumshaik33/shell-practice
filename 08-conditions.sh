0c71230b641f9690df592d582f7e3322e4393257
fayum <shaikfayum@gmail.com>
2025-09-25 16:00:48 +0530

    session

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -1,12 +1,14 @@
 #! bin/bash
 idnumber=$(id -u)
-if [ $idnumber -ne 0]; then 
+if [ $idnumber -ne 0 ]; then 
     echo "error :: place run this script with root"
+    exit 1
 fi
   dnf install mysql -y
 
 if [ $? -ne 0 ]; then 
     echo "error ::installation failed"
+    exit 1
 else 
     echo "intallation success"
 fi
