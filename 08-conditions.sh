eee91d2c0cf48ab9184af6b0624ae14e57991f7d
fayum <shaikfayum@gmail.com>
2025-09-25 13:54:54 +0530

    session

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "place enter the number"
 read number
-if [ $(($number % 2))]; then 
+if [ $(($number % 2)) ]; then 
     echo "given number $number is even"
 else
     echo "given number $number is odd"
