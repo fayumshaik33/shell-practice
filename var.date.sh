ee50acc125cd14db6ca93fe53e6ceeccf779986a
fayum <shaikfayum@gmail.com>
2025-09-25 09:47:15 +0530

    some messages

--- var.date.sh
+++ var.date.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-# Print the current date in YYYY-MM-DD format
-current_date=$(date +"%Y-%m-%d")
-echo "Today's date is: $current_date"
+start_time=$(date +%s)
+sleep 5
+end_time=$(date +%s)
+total_time=$((end_time -start_time))
+echo "the total time to execute the script is :$total_time"
