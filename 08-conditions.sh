09821eb9bc1f70060af25f05cf729c6742276989
fayum <shaikfayum@gmail.com>
2025-09-30 12:30:13 +0530

    some

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -8,8 +8,23 @@ fi
 
 if [ $? -ne 0 ]; then 
     echo "error ::installation failed"
-    exit 1
+    exit 1   
 else 
     echo "intallation success"
 fi
   
+dnf indtall nginx -y
+ if [ $? -eq 0 ]; then 
+    echo "error :: installation of nginx failed"
+    exit 1
+ else
+    echo "installation of inginx success"
+ fi
+
+ dnf install mongodb-mongosh -y
+    if [ $? -eq 0 ]; then
+        echo "error :: install of mongodb failed"
+        exit 1
+    elde 
+        echo "install of mongoadb success"
+    fi
---
+++ color.sh
@@ -0,0 +1,2 @@
+#! bin/bash
+echo "just print the what i have said to u"
